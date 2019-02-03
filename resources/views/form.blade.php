408c14862035e6690bfde5597057ff9323a682f7
amvolinec <47295538+amvolinec@users.noreply.github.com>
2019-02-03 17:04:40 +0200

    Form design

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,11 +2,21 @@
     <div class="row">
         <div class="col-md-4">&nbsp;</div>
         <div class="col-md-4">
+            <h2>Test Task Weather</h2>
             <from>
                 <div class="form-group">
-                    <label for="testApi">Email address</label>
-                    <input type="text" class="form-control" id="testApi" aria-describedby="apiHelp" placeholder="API">
-                    <small id="apiHelp" class="form-text text-muted">Enter Your API key.</small>
+                    <label for="testApi">API key:</label>
+                    <input type="text" class="form-control" id="testApi" placeholder="API">
+                </div>
+                <div class="form-group">
+                    <label for="testCity">City:</label>
+                    <div class="input-group mb-3">
+
+                        <input type="text" class="form-control" id="testCity" placeholder="City">
+                        <div class="input-group-append">
+                            <button class="btn btn-success" type="button" id="buttonSuccess"><span aria-hidden="true">&nbsp;</span></button>
+                        </div>
+                    </div>
                 </div>
             </from>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" >
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
     <title>Iterato Test Task - Weather</title>
