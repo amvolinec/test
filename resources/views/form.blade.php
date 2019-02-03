bbc31ce02e9ccdd070c56a23589443ef6ef76ff1
amvolinec <47295538+amvolinec@users.noreply.github.com>
2019-02-03 16:13:06 +0200

    Bootstrap added to App

---
+++ resources/views/form.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">&nbsp;</div>
+        <div class="col-md-4">
+            <from>
+                <div class="form-group">
+                    <label for="testApi">Email address</label>
+                    <input type="text" class="form-control" id="testApi" aria-describedby="apiHelp" placeholder="API">
+                    <small id="apiHelp" class="form-text text-muted">Enter Your API key.</small>
+                </div>
+            </from>
+        </div>
+        <div class="col-md-4">&nbsp;</div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
     <title>Iterato Test Task - Weather</title>
 
